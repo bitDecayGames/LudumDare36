a10f3947ae997deb7f34b18df72b48b699e965c7
Jacob Caban-Tomski <jacque006@gmail.com>
2016-08-27 16:42:47 -0600

    Fixed player hitbox and scale, moved fuel gauge

--- src/main/java/com/bitdecay/ludum/dare/components/PlayerAnimationComponent.java
+++ src/main/java/com/bitdecay/ludum/dare/components/PlayerAnimationComponent.java
@@ -1,6 +1,5 @@
 package com.bitdecay.ludum.dare.components;
 
-import com.badlogic.gdx.math.Vector2;
 import com.bytebreakstudios.animagic.animation.Animation;
 import com.bytebreakstudios.animagic.animation.FrameRate;
 import com.bytebreakstudios.animagic.texture.AnimagicTextureRegion;
@@ -9,10 +8,10 @@ import static com.bitdecay.ludum.dare.LudumDareGame.atlas;
 
 public class PlayerAnimationComponent extends AnimationComponent {
     public PlayerAnimationComponent(PositionComponent position) {
-        super("player", position, 1f, new Vector2());
+        super("player", position, 0.5f, null);
 
         animator.addAnimation(new Animation("run", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.1f), atlas.findRegions("player/run").toArray(AnimagicTextureRegion.class)));
-        animator.addAnimation(new Animation("jump", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("player/jetpack/idle").toArray(AnimagicTextureRegion.class)));
+        animator.addAnimation(new Animation("jump", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.1f), atlas.findRegions("player/jetpack/idle").toArray(AnimagicTextureRegion.class)));
         animator.addAnimation(new Animation("apex", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("player/jetpack/apex").toArray(AnimagicTextureRegion.class)));
         animator.addAnimation(new Animation("fall", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("player/jetpack/fall").toArray(AnimagicTextureRegion.class)));
 //        animator.addAnimation(new Animation("knockback", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("knockback").toArray(AnimagicTextureRegion.class)));
@@ -22,7 +21,7 @@ public class PlayerAnimationComponent extends AnimationComponent {
 //        animator.addAnimation(new Animation("punch/jumping/up", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("punch/jumping/up").toArray(AnimagicTextureRegion.class)));
 //        animator.addAnimation(new Animation("punch/up", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.05f), atlas.findRegions("punch/up").toArray(AnimagicTextureRegion.class)));
         animator.addAnimation(new Animation("stand", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.2f), atlas.findRegions("player/stand").toArray(AnimagicTextureRegion.class)));
-//        animator.addAnimation(new Animation("wall", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.1f), atlas.findRegions("wall").toArray(AnimagicTextureRegion.class)));
+        animator.addAnimation(new Animation("wall", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.1f), atlas.findRegions("player/jetpack/fall").toArray(AnimagicTextureRegion.class)));
 //
         animator.switchToAnimation("stand");
     }
