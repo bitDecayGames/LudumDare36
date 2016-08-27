a89eba7cc7f3b7387144f97408ff42c1c04a104e
Tristan Havelick <tristan@havelick.com>
2016-08-27 15:05:33 -0600

    Adding Aztec tiles - wip

--- src/main/java/com/bitdecay/ludum/dare/EditorLauncher.java
+++ src/main/java/com/bitdecay/ludum/dare/EditorLauncher.java
@@ -1,46 +1,19 @@
 package com.bitdecay.ludum.dare;
 
-import com.badlogic.gdx.Game;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.bitdecay.jump.leveleditor.render.LevelEditor;
-import com.bitdecay.ludum.dare.actors.player.Player;
-import com.bitdecay.ludum.dare.components.KeyboardControlComponent;
-import com.bitdecay.ludum.dare.util.Players;
-import com.bytebreakstudios.animagic.texture.AnimagicTexturePacker;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Admin on 12/13/2015.
- */
 public class EditorLauncher {
-    public static void main(String[] arg) {
-        AnimagicTexturePacker.pack(new File("sprites"), new File("packed"));
-        AnimagicTexturePacker.pack(new File("assets"), new File("packed"));
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1600;
-        config.height = 900;
-        new LwjglApplication(new Game() {
-            @Override
-            public void create() {
-                LevelEditor.TILE_SIZE = 32;
-                LevelEditor.setAssetsFolder("../../../../Jump/jump-leveleditor/assets");
-                LudumDareGame game = new LudumDareGame();
-                LudumDareGame.queueAssetsForLoad();
-                LudumDareGame.assetManager.finishLoading();
 
-                List<Player> players = new ArrayList<>();
-                Player playerInstance = new Player();
-                playerInstance.append(new KeyboardControlComponent());
-                players.add(playerInstance);
-//                players.add(playerInstance);
-//                players.add(playerInstance);
-//                players.add(playerInstance);
-                Players.initialize(players);
-            }
-        }, config);
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 900;
+        config.height = 600;
+        config.title = "Jump";
+
+        LevelEditor.setAssetsFolder("../jump/jump-leveleditor/assets");
+        new LwjglApplication(new LD36EditorApp(), config);
     }
 }
--- src/main/java/com/bitdecay/ludum/dare/screens/GameScreen.java
+++ src/main/java/com/bitdecay/ludum/dare/screens/GameScreen.java
@@ -2,9 +2,17 @@ package com.bitdecay.ludum.dare.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 import com.bitdecay.jump.collision.BitWorld;
+import com.bitdecay.jump.gdx.level.EditorIdentifierObject;
+import com.bitdecay.jump.gdx.level.RenderableLevelObject;
+import com.bitdecay.jump.level.Level;
+import com.bitdecay.jump.leveleditor.EditorHook;
 import com.bitdecay.jump.leveleditor.render.LibGDXWorldRenderer;
 import com.bitdecay.jump.leveleditor.utils.LevelUtilities;
 import com.bitdecay.ludum.dare.LudumDareGame;
@@ -14,8 +22,14 @@ import com.bitdecay.ludum.dare.cameras.FollowOrthoCamera;
 import com.bitdecay.ludum.dare.collection.GameObjects;
 import com.bitdecay.ludum.dare.components.LevelInteractionComponent;
 import com.bitdecay.ludum.dare.hud.Hud;
+import com.bytebreakstudios.animagic.texture.AnimagicTextureRegion;
 
-public class GameScreen implements Screen {
+import java.util.*;
+
+/**
+ * Created by jacob on 8/27/16.
+ */
+public class GameScreen implements Screen, EditorHook {
 
     private LudumDareGame game;
 
@@ -28,6 +42,7 @@ public class GameScreen implements Screen {
 
     private SpriteBatch uiBatch;
     private SpriteBatch gobsBatch;
+    Map<Integer, TextureRegion[]> tilesetMap = new HashMap<>();
 
     public GameScreen(LudumDareGame game) {
         this.game = game;
@@ -40,21 +55,46 @@ public class GameScreen implements Screen {
         LevelInteractionComponent playerLevelLink = new LevelInteractionComponent(world, gobs);
         player.addToScreen(playerLevelLink);
 
+        Array<AnimagicTextureRegion> aztecTileTextures = LudumDareGame.atlas.findRegions("tiles/aztec");
+        tilesetMap.put(0, aztecTileTextures.toArray(TextureRegion.class));
         world.setLevel(LevelUtilities.loadLevel(ResourceDir.path("thePit.level")));
-    }
 
-    @Override
-    public void show() {
         hud = new Hud(player);
         uiBatch = new SpriteBatch();
         gobsBatch = new SpriteBatch();
     }
 
     @Override
+    public void show() {
+    }
+
+    @Override
     public void render(float delta) {
         update(delta);
 
-        draw();
+        draw(camera);
+    }
+
+    @Override
+    public void render(OrthographicCamera cam) {
+        draw(cam);
+    }
+
+    private void draw(OrthographicCamera cam) {
+        gobsBatch.setProjectionMatrix(camera.combined);
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        worldRenderer.render(world, cam);
+
+        gobsBatch.begin();
+        gobs.draw(gobsBatch);
+        gobsBatch.end();
+
+        uiBatch.begin();
+        hud.render(uiBatch);
+        uiBatch.end();
+
     }
 
     @Override
@@ -82,27 +122,54 @@ public class GameScreen implements Screen {
 
     }
 
-    private void update (float delta) {
+    public void update(float delta) {
         world.step(delta);
         gobs.update(delta);
 
         camera.addFollowPoint(player.getPosition());
         camera.update();
 
-        gobsBatch.setProjectionMatrix(camera.combined);
+
     }
 
     private void draw() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        worldRenderer.render(world, camera);
 
-        gobsBatch.begin();
-        gobs.draw(gobsBatch);
-        gobsBatch.end();
+        // Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        uiBatch.begin();
-        hud.render(uiBatch);
-        uiBatch.end();
+        // worldRenderer.render(world, camera);
+
+        // gobsBatch.begin();
+        // gobs.draw(gobsBatch);
+        // gobsBatch.end();
+
+        // uiBatch.begin();
+        // hud.render(uiBatch);
+        // uiBatch.end();
+    }
+
+    @Override
+    public List<EditorIdentifierObject> getTilesets() {
+        return Arrays.asList(new EditorIdentifierObject(0, "Fallback", tilesetMap.get(0)[0]));
+    }
+
+    @Override
+    public List<RenderableLevelObject> getCustomObjects() {
+        List<RenderableLevelObject> exampleItems = new ArrayList<>();
+        return exampleItems;
+    }
+
+    @Override
+    public List<EditorIdentifierObject> getThemes() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public BitWorld getWorld() {
+        return world;
+    }
+
+    @Override
+    public void levelChanged(Level level) {
     }
 }
