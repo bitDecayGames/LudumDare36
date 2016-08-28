7e6f25767db8b5d62c16af40c5edd5c4abac3509
Luke Fisher <lukewfisher@msn.com>
2016-08-28 00:28:37 -0600

    added monkey animtion and scaled down.

--- src/main/java/com/bitdecay/ludum/dare/actors/ai/Monkey.java
+++ src/main/java/com/bitdecay/ludum/dare/actors/ai/Monkey.java
@@ -1,12 +1,16 @@
+// #Luke420BlazinErryDayYo
+
 package com.bitdecay.ludum.dare.actors.ai;
 
 import com.badlogic.gdx.math.Vector2;
 import com.bitdecay.jump.BodyType;
+import com.bitdecay.jump.Facing;
 import com.bitdecay.jump.JumperBody;
 import com.bitdecay.jump.collision.BitWorld;
 import com.bitdecay.jump.control.PlayerInputController;
 import com.bitdecay.jump.geom.BitRectangle;
 import com.bitdecay.jump.properties.JumperProperties;
+import com.bitdecay.jump.render.JumperRenderState;
 import com.bitdecay.jump.render.JumperRenderStateWatcher;
 import com.bitdecay.ludum.dare.actors.StateMachine;
 import com.bitdecay.ludum.dare.components.*;
@@ -32,7 +36,7 @@ public class Monkey extends StateMachine {
         size = new SizeComponent(100, 100);
         pos = new PositionComponent(0, 0);
         health = new HealthComponent(10, 10);
-        anim = new AnimationComponent("player", pos, 1f, new Vector2());
+        anim = new AnimationComponent("monkey", pos, 0.5f, new Vector2());
         setupAnimation(anim.animator);
         phys = createBody();
         input = new AIControlComponent();
@@ -45,12 +49,13 @@ public class Monkey extends StateMachine {
     private PhysicsComponent createBody() {
         JumperBody body = new JumperBody();
         body.props.deceleration = 10000;
+        body.props.maxVoluntarySpeed = 20;
         body.jumperProps = new JumperProperties();
         body.jumperProps.jumpCount = Integer.MAX_VALUE;
         body.jumperProps.jumpVariableHeightWindow = Float.POSITIVE_INFINITY;
         body.renderStateWatcher = new JumperRenderStateWatcher();
         body.bodyType = BodyType.DYNAMIC;
-        body.aabb.set(new BitRectangle(0, 0, 16, 32));
+        body.aabb.set(new BitRectangle(0, 0, 8, 8));
         body.userObject = this;
 
         setupAnimation(anim.animator);
@@ -59,7 +64,9 @@ public class Monkey extends StateMachine {
 
 
     private void setupAnimation(Animator a) {
-        a.addAnimation(new Animation("stand", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.2f), atlas.findRegions("player/stand").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("walk", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.2f), atlas.findRegions("monkey/walk").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("stand", Animation.AnimationPlayState.REPEAT, FrameRate.perFrame(0.2f), atlas.findRegions("monkey/stand").toArray(AnimagicTextureRegion.class)));
+        a.addAnimation(new Animation("jump", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.2f), atlas.findRegions("monkey/jump").toArray(AnimagicTextureRegion.class)));
         a.switchToAnimation("stand");
     }
 
@@ -67,6 +74,50 @@ public class Monkey extends StateMachine {
     public void update(float delta) {
         input.update(delta);
         super.update(delta);
+
+        updateFacing();
+
+        switch ((JumperRenderState)(phys.getBody().renderStateWatcher.getState())) {
+
+            case RIGHT_STANDING:
+            case LEFT_STANDING:
+                if (anim.animator.currentAnimationName() != "stand") {
+                    anim.animator.switchToAnimation("stand");
+                }
+                break;
+            case RIGHT_RUNNING:
+            case LEFT_RUNNING:
+                if (anim.animator.currentAnimationName() != "walk") {
+                    anim.animator.switchToAnimation("walk");
+                }
+                break;
+            case RIGHT_JUMPING:
+            case LEFT_JUMPING:
+                if (anim.animator.currentAnimationName() != "jump") {
+                    anim.animator.switchToAnimation("jump");
+                }
+                break;
+            case RIGHT_APEX:
+                break;
+            case LEFT_APEX:
+                break;
+            case RIGHT_FALLING:
+                break;
+            case LEFT_FALLING:
+                break;
+            case RIGHT_AIR_AGAINST_WALL:
+                break;
+            case LEFT_AIR_AGAINST_WALL:
+                break;
+            case RIGHT_GROUNDED_AGAINST_WALL:
+                break;
+            case LEFT_GROUNDED_AGAINST_WALL:
+                break;
+            case RIGHT_PUSHED:
+                break;
+            case LEFT_PUSHED:
+                break;
+        }
     }
 
     public void setPosition(float x, float y) {
@@ -104,5 +155,19 @@ public class Monkey extends StateMachine {
 
     public BitWorld getWorld(){
         return this.levelComponent.getWorld();
+    }
+
+    private void updateFacing() {
+        Facing facing = phys.getBody().facing;
+        switch (facing) {
+            case LEFT:
+                anim.setFlipVerticalAxis(false);
+                break;
+            case RIGHT:
+                anim.setFlipVerticalAxis(true);
+                break;
+            default:
+                throw new Error("Invalid facing set");
+        }
     }
 }
