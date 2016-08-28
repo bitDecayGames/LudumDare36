48076c90a86680874dc49a4356ad50f6350292a7
Logan Moore <monday.hopscotch@gmail.com>
2016-08-28 09:32:19 -0600

    jet packs + player stat tuning

--- src/main/java/com/bitdecay/ludum/dare/components/JetPackComponent.java
+++ src/main/java/com/bitdecay/ludum/dare/components/JetPackComponent.java
@@ -1,19 +1,20 @@
 package com.bitdecay.ludum.dare.components;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.bitdecay.jump.Facing;
 import com.bitdecay.jump.JumperBody;
 import com.bitdecay.jump.control.state.GroundedControlState;
-import com.bitdecay.jump.control.state.JumperBodyControlState;
 import com.bitdecay.jump.control.state.JumpingControlState;
-import com.bitdecay.ludum.dare.actors.state.JumpState;
 import com.bitdecay.ludum.dare.interfaces.IComponent;
+import com.bitdecay.ludum.dare.interfaces.IDraw;
 import com.bitdecay.ludum.dare.interfaces.IUpdate;
-import com.sun.org.apache.xerces.internal.parsers.CachingParserPool;
-import com.sun.org.apache.xpath.internal.SourceTree;
 
 /**
  * Created by jacob on 8/27/16.
  */
-public class JetPackComponent implements IComponent, IUpdate{
+public class JetPackComponent implements IComponent, IUpdate, IDraw {
     private JumperBody playerJumpBody;
 
     public final float maxFuel = 100;
@@ -24,20 +25,36 @@ public class JetPackComponent implements IComponent, IUpdate{
     public boolean canRefuel = false;
     public boolean firstPress = false;
 
+    ParticleEffect fx;
+
     public JetPackComponent(JumperBody playerJumpBody) {
         this.playerJumpBody = playerJumpBody;
-
+        fx = new ParticleEffect();
+        fx.load(Gdx.files.internal("particle/flame3.p"), Gdx.files.internal("particle"));
+        fx.scaleEffect(.5f);
+        fx.setPosition(playerJumpBody.aabb.xy.x, playerJumpBody.aabb.xy.y);
     }
 
     @Override
     public void update(float delta) {
+        float offset = playerJumpBody.aabb.width / 2;
+        if (playerJumpBody.facing.equals(Facing.LEFT)) {
+            offset += 5;
+        } else {
+            offset -= 5;
+        }
+        fx.setPosition(playerJumpBody.aabb.xy.x + offset, playerJumpBody.aabb.xy.y + 8);
+        fx.update(delta);
+
         if(playerJumpBody.controller.getStatus().equals(JumpingControlState.class.getSimpleName())){
             if (!isFiring) {
                 firstPress = true;
             }
             isFiring = true;
+            fx.start();
         } else {
             isFiring = false;
+            fx.allowCompletion();
         }
 
         if(playerJumpBody.controller.getStatus().equals(GroundedControlState.class.getSimpleName())) {
@@ -69,5 +86,10 @@ public class JetPackComponent implements IComponent, IUpdate{
             playerJumpBody.jumpsRemaining = Integer.MAX_VALUE;
             playerJumpBody.jumperProps.jumpVariableHeightWindow = Float.POSITIVE_INFINITY;
         }
+    }
+
+    @Override
+    public void draw(SpriteBatch spriteBatch) {
+        fx.draw(spriteBatch);
     }
 }
