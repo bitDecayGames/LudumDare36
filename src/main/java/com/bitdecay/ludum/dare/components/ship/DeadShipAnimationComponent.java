431dc423f5f5a9810c29708d35eabf55c8c41fe9
Jacob Caban-Tomski <jacque006@gmail.com>
2016-08-28 17:49:17 -0600

    Ship parts get added woo!

--- src/main/java/com/bitdecay/ludum/dare/components/ship/DeadShipAnimationComponent.java
+++ src/main/java/com/bitdecay/ludum/dare/components/ship/DeadShipAnimationComponent.java
@@ -10,10 +10,10 @@ import static com.bitdecay.ludum.dare.LudumDareGame.atlas;
 public class DeadShipAnimationComponent extends AnimationComponent {
     public static final float SCALE = 0.5f;
 
-    public DeadShipAnimationComponent() {
-        super("deadShip", null, SCALE, null);
+    public DeadShipAnimationComponent(String name) {
+        super(name, null, SCALE, null);
 
-        animator.addAnimation(new Animation("static", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("ship/pieces/deadShip").toArray(AnimagicTextureRegion.class)));
+        animator.addAnimation(new Animation("static", Animation.AnimationPlayState.ONCE, FrameRate.perFrame(0.1f), atlas.findRegions("ship/pieces/" + name).toArray(AnimagicTextureRegion.class)));
 
         animator.switchToAnimation("static");
     }
