7f47c866419c38371f4ca1aaeff5fac4c09db014
Jacob Fisher <zombiefish90@gmail.com>
2016-08-29 12:10:02 -0600

    opening cut scene is in but not hooked up

--- src/main/java/com/bitdecay/ludum/dare/LudumDareGame.java
+++ src/main/java/com/bitdecay/ludum/dare/LudumDareGame.java
@@ -23,7 +23,7 @@ public class LudumDareGame extends Game {
         assetManager.finishLoading();
         atlas = assetManager.get(ResourceDir.path("packed/main.atlas"), AnimagicTextureAtlas.class);
 
-//        setScreen(new GameScreen(this));
-        setScreen(new OpeningSceneCutScreen(this));
+        setScreen(new GameScreen(this));
+//        setScreen(new OpeningSceneCutScreen(this));
     }
 }
